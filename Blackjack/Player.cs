3111ca7bc5f8fe7f8e2b445766ea356b9d0567e2
Anna Dmitrieva <icoffee128@gmail.com>
2019-01-18 19:33:42 +0200

    add

---
+++ Blackjack/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Player
+    {
+        internal string name { get; }
+        internal List<string> cards { get; set; }
+        internal bool status { get; set; }
+        internal double money { get; set; }
+
+        internal Player(string name)
+        {
+            this.name = name;
+            cards = new List<string>();
+            money = 1000;
+            status = true;
+        }
+    }
+}
